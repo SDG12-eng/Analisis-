d61968de5316f95ce36322e8fab0ffa67f85761f
SistemaIntegralDeGestion <juniorcede3002@gmail.com>
2026-02-18 19:50:40 -0500

    Create validations.js

---
+++ validations.js
@@ -0,0 +1,6 @@
+export function initValidations() {
+    console.log("Módulo de Validaciones Cargado Correctamente.");
+    
+    // Aquí inicializaremos toda la lógica del doble Excel en el futuro.
+    // const root = document.getElementById('validations-root');
+}
