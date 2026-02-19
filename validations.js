db53090cf92f0752789e92e7e5066a1d7db74eea
SistemaIntegralDeGestion <juniorcede3002@gmail.com>
2026-02-18 20:07:17 -0500

    Delete validations.js

--- validations.js
+++
@@ -1,6 +0,0 @@
-export function initValidations() {
-    console.log("Módulo de Validaciones Cargado Correctamente.");
-    
-    // Aquí inicializaremos toda la lógica del doble Excel en el futuro.
-    // const root = document.getElementById('validations-root');
-}
